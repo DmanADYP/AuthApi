6974876b9245c15448f881d62df0cfec2e9597f6
pen11219 <pen11219@gmail.com>
2018-09-28 20:25:15 -0400

    init

---
+++ authApi/authApi.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using authApi.API.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace authApi.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+
+}
